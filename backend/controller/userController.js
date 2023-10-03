1fa2c3141f1c5346b3cef2bbd1203770324f62da
mubbashirahmad <mubbashirahmad44@gmail.com>
2023-10-04 02:02:56 +0500

    11 commit

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -96,7 +96,7 @@ class userController {
   // Update Profile
   static updateprofile = async (req, res, next) => {
     try {
-      console.log('sasasa',req);
+      console.log('sasasa',req.body,req);
       const { name, email, password} = req.body;
       // if(!req.file){
       //   res.status(500).json({message:"no file upload"})
